70784ad998797516d5cd141593afae313567337d
Fernando Simizu <AU00903405@prosegur.com>
2023-06-14 17:04:05 +1000

    added section to home page

---
+++ src/components/AboutUs/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './abousUs.css';
+
+export function AboutUs() {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <h4>ABOUT US</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi beatae delectus provident earum exercitationem. Enim numquam </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi beatae delectus provident earum exercitationem. Enim numquam ntium?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi beatae delectus provident earum exercitationem. Enim numquam ntium?</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <img id="aboutUs__image" src="/placeholder.png" alt="image about us" />
+                </div>
+            </div>
+        </div>
+)}
---
+++ src/components/OurWork/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import './ourWork.css';
+
+export function OurWork() {
+    return (
+        <div>
+            <h4>OUR WORK</h4>
+            <div class="ourWork__cards">
+                <div class="card">
+                    <img class="card-img-top" src="/placeholder.png" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img class="card-img-top" src="/placeholder.png" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img class="card-img-top" src="/placeholder.png" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+)}
