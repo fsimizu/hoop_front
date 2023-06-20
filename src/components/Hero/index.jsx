b485d58e735a5bb0af4992793ec8d77b15f2bb1d
Fernando Simizu <AU00903405@prosegur.com>
2023-06-20 17:03:15 +1000

    changes in navbar, hero, socialmedia and footer

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,15 +3,19 @@ import './hero.css';
 
 export function Hero() {
     return (
-        <div class="p-5 text-center bg-image" id="hero__container">
-            <div class="mask">
-                <div class="d-flex justify-content-center align-items-center h-100">
-                    <div class="text-white">
-                        <h1 class="mb-3">We are HOOP</h1>
-                        <h4 class="mb-3">An NGO working with communities in the northern cone of Arequipa, Perú.</h4>
-                        <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
-                    </div>
+        <div className="hero__container p-5 bg-image">
+            <div className="container">
+                <div className="d-flex justify-content-end h-100" style={{}} >
+                        <div className="text-right text-white col-lg-4">
+                            <h4 className="mb-3 hero__we-are">We are</h4>
+                            <img src="/images/hero_logo.png" alt="" />
+                            <p className="mb-3">Helping Overcome Obstacles Peru</p>
+                            <h4 className="mb-3 hero__text">An NGO working with communities in the <span class="hero__yellow-text">cono norte of Arequipa Peru.</span></h4>
+                            {/* <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a> */}
+                        </div>
                 </div>
             </div>
+
+            
         </div>
 )}
