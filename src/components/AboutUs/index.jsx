ca02cdce7b06e74c71a46e573e3a70887acaaee8
Fernando Simizu <AU00903405@prosegur.com>
2023-06-23 17:39:09 +1000

    fixed getInTouch

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -11,8 +11,8 @@ export function AboutUs() {
                     <div className="descriptionAbout">
                         <p>Hoop is a NGO dedicated to work in communities communities in the northern cone of Arequipa developing projects focused on 3 main areas: 
                         <strong> Education, Family (psychology), Community (social work)</strong></p>
-                    <p>HOOP was founded in 2012 by volunteers and staff of the Flora Tristan English School in Cerro Colorado. Families there had no access to basic services and it was sought to provide educational services to achieve a positive impact on the community.</p>
-                    <p>Today former volunteers represento us in countries like Austria, France, Germany, Taiwan, England and the United States, suportin our organization and ensuring the sustainability of our work through grants and donations.</p>
+                        <p>HOOP was founded in 2012 by volunteers and staff of the Flora Tristan English School in Cerro Colorado. Families there had no access to basic services and it was sought to provide educational services to achieve a positive impact on the community.</p>
+                        <p>Today former volunteers represento us in countries like Austria, France, Germany, Taiwan, England and the United States, suportin our organization and ensuring the sustainability of our work through grants and donations.</p>
                     </div>
                     
                 </div>
