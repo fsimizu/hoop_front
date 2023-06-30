c33e3c58399e639a9a3df82e70cf79d2ee4263b3
ezequieliribarren <ezequielgiribarren@gmail.com>
2023-06-30 19:02:50 -0300

    cambios timeline

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -21,7 +21,7 @@ const Timeline = () => {
               if (i < bottomChildDivs.length) {
                 bottomChildDivs[i].style.opacity = 1;
               }
-            }, i * 2200);
+            }, i * 1500);
           }
         };
     
@@ -33,92 +33,98 @@ const Timeline = () => {
       }, []);
     
       return (
-<section className=' timeline'>
-    <div className='topTimeline'>
+<section id='timeline' >
+  <h2>
+  Our History
+  </h2>
+  <h3>
+  Years 2012-2022</h3>
+  <div className='timeline'>
+  <div className='topTimeline'>
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
     <h4>2012</h4>  
     </div> 
 <div className='pLine'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>Creation of a little English school in Flora Tristán, Cerro Colorado, Arequipa made of 7 families and 20 children.
+ </p>
   </div>
-    <div class="line"></div>
   </div>
 
 </div>
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2014</h4>  
     </div> 
 <div className='pLine'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>Our Social Work department was created offering services in health, legal and economic advice.
+ </p>
   </div>
-    <div class="line"></div>
   </div>
 
 </div>
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2016</h4>  
     </div> 
 <div className='pLine'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>We moved to our office in Cayma. That included many social activities with volunteers. Great times!
+ </p>
   </div>
-    <div class="line"></div>
   </div>
 
 </div>
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2018</h4>  
     </div> 
 <div className='pLine'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>We were working with up to 159 children and had opened 8 English classes!
+ </p>
   </div>
-    <div class="line"></div>
   </div>
 
 </div>
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2020</h4>  
     </div> 
 <div className='pLine'>
   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
   </div>
-    <div class="line"></div>
   </div>
 
 </div>
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2022</h4>  
     </div> 
 <div className='pLine'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>Beginning of the constructions of our long awaited Community Center. Very exciting!
+</p>
   </div>
-    <div class="line"></div>
   </div>
 
 </div>
     </div>
     <div className='lineTimeline'>
-
+<img src="./images/ourWork/line.png" alt="" />
     </div>
     <div className='bottomTimeline'>
     <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2013</h4>  
     </div> 
 <div className='pLine2'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>HOOP (Helping Overcome Obstacles Peru) became an officially registered Association!
+ </p>
   </div>
 
   </div>
@@ -127,10 +133,11 @@ const Timeline = () => {
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2015</h4>  
     </div> 
 <div className='pLine2'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>Our Psychology Department was created, offering help regarding emotional and mental health.
+ </p>
   </div>
  
   </div>
@@ -139,10 +146,11 @@ const Timeline = () => {
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2017</h4>  
     </div> 
 <div className='pLine2'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>A group of HOOPsters raised 1000$ with the hula hooping on a high altitude challenge!
+</p>
   </div>
 
   </div>
@@ -151,10 +159,11 @@ const Timeline = () => {
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2019</h4>  
     </div> 
 <div className='pLine2'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>A group of HOOPsters did the <br /> “2 Peaks Challenge” Climbing the 3 highest peaks in Scotland, England, and Wales in just 24 hours!
+ </p>
   </div>
 
   </div>
@@ -163,16 +172,20 @@ const Timeline = () => {
 <div class="parent">
   <div class="child">    
     <div className='yearTimeline'>
-    <h4>2012</h4>  
+    <h4>2021</h4>  
     </div> 
 <div className='pLine2'>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam amet fugiat fugit? </p>
+  <p>We updated our brand logo, our objectives, our mission and vision as well.
+</p>
   </div>
  
   </div>
 
 </div>
     </div>
+  </div>
+  
+  
 </section>
   )
 }
