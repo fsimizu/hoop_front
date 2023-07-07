b958e30ad5d2af99ccd73ce9fdf69e2ba984bacf
ezequieliribarren <ezequielgiribarren@gmail.com>
2023-07-07 14:25:58 -0300

    ourImpact-reports

--- src/components/OurImpact/index.jsx
+++ src/components/OurImpact/index.jsx
@@ -4,54 +4,13 @@ import './ourImpact.css';
 
 export function OurImpact() {
     return (
-        // <div className="ourImpact__container section__top-margin">
-        //     <h2 className="section__header">Our impact</h2>
-        //     <img className="section__underline" src="/images/underline.svg" alt="" />
-        //     <div className="text-main text-main-container">
-        //         Learn how these projects have impacted the community by reading these reports.
-        //     </div>
-
-        //     <div className="ourImpact__cards justify-content-center">
-        //         <div className="card">
-        //             <div className="ourImpact__card-content background-gray overflow-hidden ">
-        //                 <img src="/images/ourImpact_icon1.svg" alt="" />
-        //                 <h2>Report 1</h2>
-        //             </div>
-        //             <div>
-        //                 <a href="#" className="ourImpact__readMore btn btn-secondary">Read more</a>
-        //             </div>
-        //         </div>
-
-        //         <div className="card">
-        //             <div className="ourImpact__card-content background-gray overflow-hidden ">
-        //                 <img src="/images/ourImpact_icon2.svg" alt="" />
-        //                 <h2>Report 2</h2>
-        //             </div>
-        //             <div>
-        //                 <a href="#" className="ourImpact__readMore btn btn-secondary">Read more</a>
-        //             </div>
-        //         </div>
-
-        //         <div className="card">
-        //             <div className="ourImpact__card-content background-gray overflow-hidden ">
-
-        //                 <img src="/images/ourImpact_icon3.svg" alt="" />
-        //                 <h2>Report 3</h2>
-
-        //             </div>
-        //             <div>
-        //                 <a href="#" className="ourImpact__readMore btn btn-secondary">Read more</a>
-        //             </div>
-        //         </div>
-        //     </div>
-
-        // </div>
-
-        <div >
+        <div className="ourImpact">
              <h2 className="section__header">Our impact</h2>
              <img className="section__underline" src="/images/underline.svg" alt="" />
-             <div className="hoopReportsImg"> 
-             <a href="#"> <img src="./images/hoopReports.png" alt="Hoop Reports" />    </a>
+             <div className="hoopReports"> 
+             <div className="hoopReportsImg">
+                <a href="#"> <img src="./images/hoopReports.png" alt="Hoop Reports" /></a> 
+             </div>
              </div>
         </div>
 )}
