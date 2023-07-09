11f5c35395a4a4b1dfa0c251ebcddd5cb6b409db
Fernando Simizu <AU00903405@prosegur.com>
2023-07-09 22:46:53 +1000

    more fixes

--- src/components/OurImpact/index.jsx
+++ src/components/OurImpact/index.jsx
@@ -47,11 +47,12 @@ export function OurImpact() {
 
         // </div>
 
-        <div >
+        <div className="container">
              <h2 className="section__header">Our impact</h2>
              <img className="section__underline" src="/images/underline.svg" alt="" />
+             <p className="text-main">Learn how these projects have impacted the community by reading these reports.</p>
              <div className="hoopReportsImg"> 
-             <a href="#"> <img src="./images/hoopReports.png" alt="Hoop Reports" />    </a>
+                <a href="#"><img src="./images/hoopReports.png" alt="Hoop Reports" /></a>
              </div>
         </div>
 )}
